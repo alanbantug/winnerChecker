b54a25cf2fe2cc9fd3b47ea2ef41c24eebf5ccca
Alan Bantug <ab5383.gmail.com>
2017-10-06 13:49:33 -0700

    Add account number entry and display fields to finalize GUI screen layout.

--- fwc.py
+++ fwc.py
@@ -35,13 +35,18 @@ class Application(Frame):
         self.getMatch3 = IntVar()
         self.getMatch4 = IntVar()
         self.getMatch5 = IntVar()
+        self.numberA = StringVar()
+        self.numberB = StringVar()
+        self.numberC = StringVar()
+        self.numberD = StringVar()
+        self.numberE = StringVar()
         
         # Create main frame
         self.main_container.grid(column=0, row=0, sticky=(N,S,E,W))
 
         # Set Label styles
         Style().configure("M.TLabel", font="Courier 20 bold", height="20", foreground="blue", background="white", anchor="center")
-        Style().configure("B.TLabel", font="Verdana 8", background="white", width="40")
+        Style().configure("B.TLabel", font="Verdana 8", background="white", width="38")
         Style().configure("MS.TLabel", font="Verdana 10" )
         Style().configure("S.TLabel", font="Verdana 8" )
         Style().configure("G.TLabel", font="Verdana 8")
@@ -53,6 +58,8 @@ class Application(Frame):
         Style().configure("B.TCheckbutton", font="Verdana 8")
 
         Style().configure("L.TListbox", font="Verdana 8", width="40")
+        Style().configure("E.TEntrybox", width="10")
+
         
         Style().configure("O.TLabelframe.Label", font="Verdana 8", foreground="black")
         
@@ -63,31 +70,39 @@ class Application(Frame):
         self.sep_d = Separator(self.main_container, orient=HORIZONTAL)
         self.sep_e = Separator(self.main_container, orient=HORIZONTAL)
         self.sep_f = Separator(self.main_container, orient=HORIZONTAL)
-        self.mainLabel = Label(self.main_container, text="FANTASY FIVE WINNER CHECKER", style="M.TLabel" )
+        self.mainLabel = Label(self.main_container, text="FANTASY FIVE CHECKER", style="M.TLabel" )
         self.subLabelA = Label(self.main_container, text="Checks a Fantasy Five combination if it is a winner based on a selected text file ", style="S.TLabel" )
         self.subLabelB = Label(self.main_container, text="downloaded from CALottery.com that contains all prior winners of the game. ", style="S.TLabel" )
         self.subLabelC = Label(self.main_container, text="Winners that match 3 or 4 numbers from the combination entered are also listed.", style="S.TLabel" )
+
+        self.numberEntry = LabelFrame(self.main_container, text=' Combination ', style="O.TLabelframe")
+        self.submit = Button(self.numberEntry, text="CHECK", style="B.TButton", width='22', command=self.startProcess)
+        self.numA = Entry(self.numberEntry, textvariable=self.numberA, width="6")
+        self.numB = Entry(self.numberEntry, textvariable=self.numberB, width="6")
+        self.numC = Entry(self.numberEntry, textvariable=self.numberC, width="6")
+        self.numD = Entry(self.numberEntry, textvariable=self.numberD, width="6")
+        self.numE = Entry(self.numberEntry, textvariable=self.numberE, width="6")
 
         self.filterOpt = LabelFrame(self.main_container, text=' Filter Options ', style="O.TLabelframe")
         self.match3 = Checkbutton(self.filterOpt, text=' Match 3 Numbers ', style="B.TCheckbutton", variable=self.getMatch3)
         self.match4 = Checkbutton(self.filterOpt, text=' Match 4 Numbers ', style="B.TCheckbutton", variable=self.getMatch4)
         self.match5 = Checkbutton(self.filterOpt, text=' Match 5 Numbers ', style="B.TCheckbutton", variable=self.getMatch5)
 
-        self.dataDisplay = LabelFrame(self.main_container, text=' Combinations ', style="O.TLabelframe")
+        self.dataDisplay = LabelFrame(self.main_container, text=' Winner Matches ', style="O.TLabelframe")
         self.scroller = Scrollbar(self.dataDisplay, orient=VERTICAL)
-        self.dataSelect = Listbox(self.dataDisplay, yscrollcommand=self.scroller.set)
+        self.dataSelect = Listbox(self.dataDisplay, yscrollcommand=self.scroller.set, width=70)
+        #self.dataSelect = Listbox(self.dataDisplay)
         
         self.dataOpt = LabelFrame(self.main_container, text=' Data File Options ', style="O.TLabelframe")
-        self.dataSource = Button(self.dataOpt, text="Data File", style="B.TButton", command=self.setSource)
+        self.dataSource = Button(self.dataOpt, text="DATA FILE", style="B.TButton", width=22, command=self.setSource)
         self.dataLabel = Label(self.dataOpt, text="None", style="B.TLabel" )
 
         #self.sep_s = Separator(self.sourceTarget, orient=HORIZONTAL)
         #self.sep_t = Separator(self.sourceTarget, orient=HORIZONTAL)
         
         self.statusLabel = Label(self.main_container, text="Select source and target folders", style="G.TLabel")
-        self.submit = Button(self.main_container, text="CHECK", style="B.TButton", command=self.startProcess)
-        self.reset = Button(self.main_container, text="RESET", style="B.TButton", command=self.restartProcess)
-        self.exit = Button(self.main_container, text="EXIT", style="B.TButton", command=root.destroy)
+        self.reset = Button(self.main_container, text="RESET", style="B.TButton", width=30, command=self.restartProcess)
+        self.exit = Button(self.main_container, text="EXIT", style="B.TButton", width=30, command=root.destroy)
 
         self.progress_bar = Progressbar(self.main_container, orient="horizontal", mode="indeterminate", maximum=50)
         
@@ -99,28 +114,38 @@ class Application(Frame):
 
         self.sep_a.grid(row=4, column=0, columnspan=3, padx=5, pady=5, sticky='NSEW')
 
-        self.dataSelect.grid(row=0, column=0, padx=5, pady=5, sticky='NSEW')
-        self.scroller.grid(row=0, column=2, sticky='NSEW')
-        self.dataDisplay.grid(row=5, column=0, columnspan=3, rowspan=5, padx=5, pady=5, sticky='NSEW')
+        self.numA.grid(row=0, column=0, padx=(10,0), pady=(5, 10), sticky='W')
+        self.numB.grid(row=0, column=0, padx=(65,0), pady=(5, 10), sticky='W')
+        self.numC.grid(row=0, column=0, padx=(120,0), pady=(5, 10), sticky='W')
+        self.numD.grid(row=0, column=0, padx=(175,0), pady=(5, 10), sticky='W')
+        self.numE.grid(row=0, column=0, padx=(230,0), pady=(5, 10), sticky='W')
+        self.submit.grid(row=0, column=0, padx=(290,0), pady=(5, 10), sticky='NSEW')
+        self.numberEntry.grid(row=5, column=0, columnspan=3, padx=5, pady=5, sticky='NSEW')
 
-        self.dataSource.grid(row=0, column=0, columnspan=1, padx=5, pady=5, sticky='NSEW')
-        self.dataLabel.grid(row=0, column=1, columnspan=2, padx=5, pady=5, sticky='NSEW')
-        self.dataOpt.grid(row=10, column=0, columnspan=3, padx=5, pady=5, sticky='NSEW')
+        self.sep_b.grid(row=6, column=0, columnspan=3, padx=5, pady=5, sticky='NSEW')
 
-        self.match3.grid(row=0, column=0, padx=5, pady=5, sticky='W')
-        self.match4.grid(row=0, column=1, padx=5, pady=5, sticky='W')
-        self.match5.grid(row=0, column=2, padx=5, pady=5, sticky='W')
-        self.filterOpt.grid(row=11, column=0, columnspan=3, padx=5, pady=5, sticky='NSEW')
+        self.dataSelect.grid(row=0, column=0, padx=(10,0), pady=5, sticky='NSEW')
+        self.scroller.grid(row=0, column=2, padx=(10,0), pady=5, sticky='NSEW')
+        self.dataDisplay.grid(row=7, column=0, columnspan=3, rowspan=5, padx=5, pady=5, sticky='NSEW')
+
+        self.dataSource.grid(row=0, column=0, padx=(10, 0), pady=(5, 10), sticky='W')
+        self.dataLabel.grid(row=0, column=0, padx=(180,0), pady=(5, 10), sticky='W')
+        self.dataOpt.grid(row=12, column=0, columnspan=3, padx=5, pady=5, sticky='NSEW')
+
+        self.match3.grid(row=0, column=0, padx=10, pady=5, sticky='W')
+        self.match4.grid(row=0, column=1, padx=10, pady=5, sticky='W')
+        self.match5.grid(row=0, column=2, padx=10, pady=5, sticky='W')
+        self.filterOpt.grid(row=13, column=0, columnspan=3, padx=5, pady=5, sticky='NSEW')
         
-        self.sep_b.grid(row=12, column=0, columnspan=3, padx=5, pady=5, sticky='NSEW')
+        self.sep_c.grid(row=14, column=0, columnspan=3, padx=5, pady=5, sticky='NSEW')
 
-        self.submit.grid(row=13, column=0, columnspan=1, padx=5, pady=0, sticky='NSEW')
-        self.reset.grid(row=13, column=1, padx=5, pady=0, sticky='NSEW')
-        self.exit.grid(row=13, column=2, padx=5, pady=0, sticky='NSEW')
-        self.sep_d.grid(row=14, column=0, columnspan=3, padx=5, pady=5, sticky='NSEW')
+        self.reset.grid(row=15, column=0, padx=(10, 0), pady=5, sticky='W')
+        self.exit.grid(row=15, column=0, padx=(245, 0), pady=5, sticky='W')
+
+        self.sep_d.grid(row=16, column=0, columnspan=3, padx=5, pady=5, sticky='NSEW')
         #self.statusLabel.grid(row=15, column=0, columnspan=3, padx=5, pady=0, sticky='NSEW')
-        self.sep_e.grid(row=15, column=0, columnspan=3, padx=5, pady=5, sticky='NSEW')
-        self.progress_bar.grid(row=16, column=0, columnspan=3, padx=5, pady=0, sticky='NSEW')
+        #self.sep_e.grid(row=15, column=0, columnspan=3, padx=5, pady=5, sticky='NSEW')
+        self.progress_bar.grid(row=17, column=0, columnspan=3, padx=5, pady=5, sticky='NSEW')
 
         self.getMatch3.set(0)
         self.getMatch4.set(0)
@@ -343,13 +368,13 @@ class Application(Frame):
         self.copying = 0
 
 root = Tk()
-root.title("FOLDER COPY UTILITY")
+root.title("FANTASY FIVE CHECKER")
 #root.minsize(480, 380)
 #root.maxsize(480, 380)
 
 # Set size
 
-wh = 520
+wh = 600
 ww = 480
 
 #root.resizable(height=False, width=False)
